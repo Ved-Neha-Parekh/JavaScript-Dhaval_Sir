0189c14de772b36f65487437d37e9dfdf4d67e58
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-07-06 19:50:46 +0530

    Chart Js done

---
+++ Day_8_Chart_JS/chart_2.js
@@ -0,0 +1,45 @@
+const ctx_2 = document.getElementById("doughnut-chart");
+
+new Chart(ctx_2, {
+  type: "doughnut",
+  data: {
+    labels: [
+      "North Region",
+      "South Region",
+      "East Region",
+      "West Region",
+      "Central Region",
+      "International",
+    ],
+    datasets: [
+      {
+        label: "Regional Sales Data",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(256, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
