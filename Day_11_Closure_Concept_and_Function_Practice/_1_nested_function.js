11414939d4e7b7254f20213822325962368b8f9e
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-07-13 21:22:05 +0530

    nested function prog-1

---
+++ Day_11_Closure_Concept_and_Function_Practice/_1_nested_function.js
@@ -0,0 +1,33 @@
+// function outerFunction() {
+//   let outerVar = "I am Outer";
+
+//   function innerFunction() {
+//     console.log(outerVar);
+//     console.log("I am inner function");
+//   }
+
+//   function secondInner() {
+//     console.log("Hello Second Inner here..");
+//   }
+
+//   secondInner();
+//   innerFunction();
+// }
+// outerFunction();
+
+function name() {
+  let name = "Ved";
+  function greet() {
+    console.log("Hello " + name);
+  }
+
+  function prtMsg() {
+    console.log("You are amazing!");
+  }
+
+  prtMsg();
+
+  greet();
+}
+
+name();
