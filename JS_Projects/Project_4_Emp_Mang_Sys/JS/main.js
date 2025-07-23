daefbfd25d1702137d7e3748f0c45e8bd2db3c48
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-07-23 19:08:45 +0530

    EMS Ui Done ✔ JS left ❌

---
+++ JS_Projects/Project_4_Emp_Mang_Sys/JS/main.js
@@ -0,0 +1,5 @@
+let empNameInput = document.getElementById("empName");
+let empRoleInput = document.getElementById("empRole");
+let searchBox = document.getElementById("searchBox");
+let empTableBody = document.getElementById("empTableBody");
+let emptyMsg = document.getElementById("emptyMsg");
