556d67d146ef2694bf56348c25acb90018f68ccd
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-07-24 18:46:28 +0530

    EMS Add Employee done

--- JS_Projects/Project_4_Emp_Mang_Sys/JS/main.js
+++ JS_Projects/Project_4_Emp_Mang_Sys/JS/main.js
@@ -14,32 +14,32 @@ function renderEmployees() {
     return;
   } else {
     emptyMsg.style.display = "none";
-    for (let i = 0; i < employees.length, i++; ) {
-      const employee = employees[i];
-      const row = document.createElement("tr");
-      const nameCell = document.createElement("td");
-      nameCell.textContent = employee.name;
-      row.appendChild(nameCell);
-      const roleCell = document.createElement("td");
-      roleCell.textContent = employee.role;
-      row.appendChild(roleCell);
-      const actionCell = document.createElement("td");
+  }
+  for (let i = 0; i < employees.length; i++) {
+    const employee = employees[i];
+    const row = document.createElement("tr");
+    const nameCell = document.createElement("td");
+    nameCell.textContent = employee.name;
+    row.appendChild(nameCell);
+    const roleCell = document.createElement("td");
+    roleCell.textContent = employee.role;
+    row.appendChild(roleCell);
+    const actionCell = document.createElement("td");
 
-      const editBtn = document.createElement("button");
-      editBtn.innerHTML = '<i class="fa-solid fa-edit"></i> Edit';
-      editBtn.classList.add("btn", "btn-sm", "btn-info", "btn-edit");
-      editBtn.setAttribute("data-id", employee.id);
-      actionCell.appendChild(editBtn);
+    const editBtn = document.createElement("button");
+    editBtn.innerHTML = '<i class="fa-solid fa-edit"></i> Edit';
+    editBtn.classList = "btn btn-sm btn-info btn-edit";
+    editBtn.setAttribute("data-id", employee.id);
+    actionCell.appendChild(editBtn);
 
-      const deleteBtn = document.createElement("button");
-      deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i> Delete';
-      deleteBtn.classList.add("btn", "btn-sm", "btn-danger", "btn-delete");
-      deleteBtn.setAttribute("data-id", employee.id);
-      actionCell.appendChild(deleteBtn);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i> Delete';
+    deleteBtn.classList = "btn  btn-sm btn-danger btn-delete ms-3";
+    deleteBtn.setAttribute("data-id", employee.id);
+    actionCell.appendChild(deleteBtn);
 
-      row.appendChild(actionCell);
-      empTableBody.appendChild(row);
-    }
+    row.appendChild(actionCell);
+    empTableBody.appendChild(row);
   }
 }
 
@@ -50,20 +50,22 @@ empForm.addEventListener("submit", function (event) {
 
   if (employeeName === "" || employeeRole === "") {
     alert("Please fill out this form....");
-  } else {
-    const newEmployeeId = nextId++;
-
-    const newEmployee = {
-      id: newEmployeeId,
-      name: employeeName,
-      role: employeeRole,
-    };
-
-    employees.push(newEmployee);
-    renderEmployees();
-    empNameInput = "";
-    empRoleInput = "";
+    return;
   }
-});z
+
+  const newEmployeeId = nextId++;
+
+  const newEmployee = {
+    id: newEmployeeId,
+    name: employeeName,
+    role: employeeRole,
+  };
+
+  employees.push(newEmployee);
+  empNameInput.value = "";
+  empRoleInput.value = "";
+
+  renderEmployees();
+});
 
 renderEmployees();
